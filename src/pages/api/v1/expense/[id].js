a698228d8d3b67fbf082570e8ea1a007a088198c
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2025-02-05 21:50:33 +0000

    feat: implement `post` route for `expense` feature

---
+++ src/pages/api/v1/expense/[id].js
@@ -0,0 +1,58 @@
+import { createRouter } from "next-connect";
+import prisma from "@infra/database";
+import {
+  onInternalServerErrorHandler,
+  onNoMatchHandler,
+} from "helpers/handlers";
+import { httpSuccessCreated, httpSuccessUpdated } from "helpers/httpSuccess";
+
+const route = createRouter();
+
+route.post(postHandler);
+
+export default route.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onInternalServerErrorHandler,
+});
+
+async function postHandler(req, res) {
+  const query = req.query;
+  const bankStatementId = parseInt(query.id);
+  const { name, description, total, bankId } = JSON.parse(req.body);
+
+  await prisma.bankStatement.update({
+    where: {
+      id: bankStatementId,
+    },
+    data: {
+      expenses: {
+        create: {
+          name,
+          description,
+          total,
+          bankId,
+        },
+      },
+    },
+    include: {
+      expenses: true,
+    },
+  });
+  const totalExpenses = await prisma.expense.aggregate({
+    where: { bankStatementId: bankStatementId },
+    _sum: { total: true },
+  });
+  const totalExpenseValue = totalExpenses._sum.total || 0;
+
+  await prisma.bankStatement.update({
+    where: { id: bankStatementId },
+    data: {
+      balanceTotal: {
+        decrement: totalExpenseValue,
+      },
+    },
+  });
+
+  const responseSuccess = new httpSuccessCreated(`expense created`);
+  return res.status(responseSuccess.statusCode).json(responseSuccess);
+}
