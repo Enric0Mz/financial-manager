4badf6bee34ccf9be03e8407dc9653fa62d0264a
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2025-02-21 00:05:30 +0000

    fix: remove redundant field for `bankStatement` table

---
+++ prisma/migrations/20250220235430_remove_filed_initial_balance/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `initialBalance` on the `BankStatement` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BankStatement" DROP COLUMN "initialBalance";
