f079f176bdd0619c7c9c4799b6fc252711f39968
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-24 10:38:57 -0300

    feat: implement `models` with year

---
+++ src/models/year.js
@@ -0,0 +1,38 @@
+import prisma from "@infra/database";
+
+async function findAll() {
+  return await prisma.year.findMany();
+}
+
+async function findUnique(id) {
+  return await prisma.year.findUnique({
+    where: {
+      yearNumber: id,
+    },
+  });
+}
+
+async function create(id) {
+  return await prisma.year.create({
+    data: {
+      yearNumber: id,
+    },
+  });
+}
+
+async function remove(id) {
+  return await prisma.year.delete({
+    where: {
+      yearNumber: id,
+    },
+  });
+}
+
+const year = {
+  findAll,
+  findUnique,
+  create,
+  remove,
+};
+
+export default year;
