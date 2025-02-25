d695384c7cc63f8e85ef0c717ff2930b49240baf
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-25 14:49:45 -0300

    refactor: refactor `month` post tests with describe

--- src/tests/integration/api/v1/month/post.test.js
+++ src/tests/integration/api/v1/month/post.test.js
@@ -1,44 +1,61 @@
-import setupDatabase from "tests/setupTests";
+import setup from "tests/setupDatabase.js";
+import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-  await setupDatabase();
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const year = 2025;
+  setup.createYear(year);
 });
 
-test("route POST /api/v1/month should return 201 created and message 'all months created successufuly'", async () => {
-  const response = await fetch(`${process.env.BASE_API_URL}/month`, {
-    method: "POST",
+describe("POST /api/v1/month", () => {
+  describe("Anonymous user", () => {
+    test("Creating all months of the year", async () => {
+      const response = await fetch(`${process.env.BASE_API_URL}/month`, {
+        method: "POST",
+      });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(201);
+      expect(responseBody.message).toBe("all months created successufuly");
+    });
+
+    test("Creating month january on year 2025", async () => {
+      const year = 2025;
+      const month = "january";
+      const response = await fetch(
+        `${process.env.BASE_API_URL}/month/${year}`,
+        {
+          method: "POST",
+          body: month,
+        },
+      );
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(201);
+      expect(responseBody.name).toBe("created");
+      expect(responseBody.message).toBe(`month ${month} created on ${year}`);
+    });
+
+    test("Trying to insert month in non existent year", async () => {
+      const year = 2020;
+      const month = "january";
+      const responseWithExpectedError = await fetch(
+        `${process.env.BASE_API_URL}/month/${year}`,
+        {
+          method: "POST",
+          body: month,
+        },
+      );
+      const responseBodyWithExpectedError =
+        await responseWithExpectedError.json();
+
+      expect(responseWithExpectedError.status).toBe(404);
+      expect(responseBodyWithExpectedError.name).toBe("not found");
+      expect(responseBodyWithExpectedError.message).toBe(
+        `Value ${year} does not exist on table. Try another value`,
+      );
+    });
   });
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(201);
-  expect(responseBody.message).toBe("all months created successufuly");
-});
-
-test("route POST /api/v1/month/2025 should return created 201 and message 'month january created on 2025'", async () => {
-  const response = await fetch(`${process.env.BASE_API_URL}/month/2025`, {
-    method: "POST",
-    body: "january",
-  });
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(201);
-  expect(responseBody.name).toBe("created");
-  expect(responseBody.message).toBe("month january created on 2025");
-});
-
-test("route POST /api/v1/month/2020 should return created 404 not found if yaer 2020 do not exist", async () => {
-  const responseWithExpectedError = await fetch(
-    `${process.env.BASE_API_URL}/month/2020`,
-    {
-      method: "POST",
-      body: "january",
-    },
-  );
-  const responseBodyWithExpectedError = await responseWithExpectedError.json();
-
-  expect(responseWithExpectedError.status).toBe(404);
-  expect(responseBodyWithExpectedError.name).toBe("not found");
-  expect(responseBodyWithExpectedError.message).toBe(
-    "Value 2020 does not exist on table. Try another value",
-  );
 });
