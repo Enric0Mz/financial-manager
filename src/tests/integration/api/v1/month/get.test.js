79b5221511a41ed81d40f7cbb6cabdd40c18f1b7
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-26 15:37:39 -0300

    refactor: use describe for more robust `month` tests

--- src/tests/integration/api/v1/month/get.test.js
+++ src/tests/integration/api/v1/month/get.test.js
@@ -1,17 +1,28 @@
-import setupDatabase from "tests/setupTests";
+import orchestrator from "tests/orchestrator";
+import setup from "tests/setupDatabase";
 
 beforeAll(async () => {
-  await setupDatabase({
-    createMonths: ["january"],
-  });
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const year = 2025;
+  const month = "January";
+  await setup.createAllMonths();
+  await setup.createYear(year);
+  await setup.createMonthInYear(month, year);
 });
 
-test("route GET api/v1/month/2025 should return 200 with a object with a list of months", async () => {
-  const response = await fetch(`${process.env.BASE_API_URL}/month/2025`);
-  const responseBody = await response.json();
+describe("GET api/v1/month", () => {
+  describe("Anonymous user", () => {
+    test("Fething months", async () => {
+      const year = 2025;
+      const response = await fetch(`${process.env.BASE_API_URL}/month/${year}`);
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody.data)).toBe(true);
-  expect(responseBody.data[0].month).toBe("January");
-  expect(responseBody.data[0].numeric).toBe(1);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(responseBody.data)).toBe(true);
+      expect(responseBody.data[0].month).toBe("January");
+      expect(responseBody.data[0].numeric).toBe(1);
+    });
+  });
 });
