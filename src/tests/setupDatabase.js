9483ff7c3965dbfa786529672aff2638bd6dfa02
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-26 15:36:20 -0300

    feat: include new methods to perform `setup`

--- src/tests/setupDatabase.js
+++ src/tests/setupDatabase.js
@@ -1,13 +1,25 @@
 // This file will override  setupTests
 
 import year from "models/year.js";
+import month from "models/month";
+import yearMonth from "models/yearMonth";
 
 async function createYear(yearId) {
   return await year.create(yearId);
 }
 
+async function createAllMonths() {
+  return await month.createAllMonths();
+}
+
+async function createMonthInYear(month, year) {
+  return await yearMonth.create(month, year);
+}
+
 const setup = {
   createYear,
+  createAllMonths,
+  createMonthInYear,
 };
 
 export default setup;
