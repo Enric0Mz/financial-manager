36af7b2d4ea57a772a168c6c55fe8faf2a85af5d
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-27 16:48:48 -0300

    feat: include `findFirst` and `update` methods in model

--- src/models/salary.js
+++ src/models/salary.js
@@ -1,5 +1,13 @@
 import prisma from "infra/database.js";
 
+async function findFirst() {
+  return await prisma.salary.findFirst({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
+
 async function create(amount) {
   await prisma.salary.create({
     data: {
@@ -8,8 +16,21 @@ async function create(amount) {
   });
 }
 
+async function update(id, amount) {
+  await prisma.salary.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      amount: amount,
+    },
+  });
+}
+
 const salary = {
   create,
+  findFirst,
+  update,
 };
 
 export default salary;
