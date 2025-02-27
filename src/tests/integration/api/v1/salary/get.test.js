89dfe553c052518f50b236930de0e2aac157b8b7
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-27 16:50:31 -0300

    refactor: use describe for more robust `salary` `put` and `get` tests

--- src/tests/integration/api/v1/salary/get.test.js
+++ src/tests/integration/api/v1/salary/get.test.js
@@ -1,22 +1,25 @@
-import setupDatabase from "tests/setupTests";
+import setup from "tests/setupDatabase.js";
+import orchestrator from "tests/orchestrator";
+
+const secondSalary = 4550.12;
 
 beforeAll(async () => {
-  await setupDatabase({
-    createSalary: {
-      create: true,
-      value: 3000.12,
-    },
-  });
-  await fetch(`${process.env.BASE_API_URL}/salary`, {
-    method: "POST",
-    body: JSON.stringify({ amount: 4550.12 }),
-  }); // TODO: fix this logic
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const firstSalary = 3000.12;
+  await setup.createSalary(firstSalary);
+  await setup.createSalary(secondSalary);
 });
 
-test("route GET api/v1/salary should return a object with the most recent created salary", async () => {
-  const response = await fetch(`${process.env.BASE_API_URL}/salary`);
-  const responseBody = await response.json();
+describe("GET api/v1/salary", () => {
+  describe("Anonymous user", () => {
+    test("Getting most recend salary added", async () => {
+      const response = await fetch(`${process.env.BASE_API_URL}/salary`);
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(responseBody.data.salary).toEqual(4550.12);
+      expect(response.status).toBe(200);
+      expect(responseBody.data.salary).toEqual(secondSalary);
+    });
+  });
 });
