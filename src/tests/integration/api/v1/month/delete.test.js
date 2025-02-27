2a3168470618f8e07c89e4b52dd25d78cbf27c73
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-27 09:26:29 -0300

    refactor: use describe for more robust `month` `delete` tests

--- src/tests/integration/api/v1/month/delete.test.js
+++ src/tests/integration/api/v1/month/delete.test.js
@@ -1,48 +1,55 @@
-import setupDatabase from "tests/setupTests";
+import orchestrator from "tests/orchestrator";
+import setup from "tests/setupDatabase";
 
 beforeAll(async () => {
-  await setupDatabase({
-    createMonths: ["january"],
-  });
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const year = 2025;
+  const month = "January";
+  await setup.createAllMonths();
+  await setup.createYear(year);
+  await setup.createMonthInYear(month, year);
 });
 
-test("route DELETE api/v1/month/2025 with body january should return 200 deleted", async () => {
-  const response = await fetch(`${process.env.BASE_API_URL}/month/2025`, {
-    method: "DELETE",
-    body: "january",
+describe("DELETE api/v1/month", () => {
+  describe("Anonymous user", () => {
+    test("Deleting month", async () => {
+      const response = await fetch(`${process.env.BASE_API_URL}/month/2025`, {
+        method: "DELETE",
+        body: "january",
+      });
+
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(responseBody.name).toBe("deleted");
+      expect(responseBody.message).toBe("value january deleted sucessfuly");
+    });
+
+    test("Trying to delete month that does not exist", async () => {
+      const response = await fetch(`${process.env.BASE_API_URL}/month/2025`, {
+        method: "DELETE",
+        body: "december",
+      });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(404);
+      expect(responseBody.name).toBe("not found");
+      expect(responseBody.message).toBe(
+        "Value december does not exist on table. Try another value",
+      );
+    });
+
+    test("Trying to delete month that does not exist on year", async () => {
+      const response = await fetch(`${process.env.BASE_API_URL}/month/9999`, {
+        method: "DELETE",
+        body: "january",
+      });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(404);
+      expect(responseBody.name).toBe("not found");
+    });
   });
-
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(200);
-  expect(responseBody.name).toBe("deleted");
-  expect(responseBody.message).toBe("value january deleted sucessfuly");
-});
-
-test("route DELETE api/v1/month/2025 with body december or invalid year should return 404 not found", async () => {
-  const responseWithInvalidYear = await fetch(
-    `${process.env.BASE_API_URL}/month/9999`,
-    {
-      method: "DELETE",
-      body: "january",
-    },
-  );
-  const responseBodyWithInvalidYear = await responseWithInvalidYear.json();
-
-  const responseWithInvalidMonth = await fetch(
-    `${process.env.BASE_API_URL}/month/2025`,
-    {
-      method: "DELETE",
-      body: "december",
-    },
-  );
-  const responseBodyWithInvalidMonth = await responseWithInvalidMonth.json();
-
-  expect(responseWithInvalidMonth.status).toBe(404);
-  expect(responseWithInvalidYear.status).toBe(404);
-  expect(responseBodyWithInvalidMonth.name).toBe("not found");
-  expect(responseBodyWithInvalidYear.name).toBe("not found");
-  expect(responseBodyWithInvalidMonth.message).toBe(
-    "Value december does not exist on table. Try another value",
-  );
 });
