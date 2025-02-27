671b68219c5199864c6881164965cc9a66ed61f2
Enrico Marquez <enricovmarquezz@gmail.com>
2025-02-27 16:49:43 -0300

    feat: include `createSalary` to setup tests

--- src/tests/setupDatabase.js
+++ src/tests/setupDatabase.js
@@ -3,6 +3,7 @@
 import year from "models/year.js";
 import month from "models/month";
 import yearMonth from "models/yearMonth";
+import salary from "models/salary";
 
 async function createYear(yearId) {
   return await year.create(yearId);
@@ -16,10 +17,15 @@ async function createMonthInYear(month, year) {
   return await yearMonth.create(month, year);
 }
 
+async function createSalary(amount) {
+  return await salary.create(amount);
+}
+
 const setup = {
   createYear,
   createAllMonths,
   createMonthInYear,
+  createSalary,
 };
 
 export default setup;
