fe1129cc7c07fef43ebe83e9c52c9c8a02932d82
Enrico Marquez <enricovmarquezz@gmail.com>
2025-03-06 10:44:51 -0300

    refactor: use describe for more robust `bank` `put` tests

--- src/tests/integration/api/v1/bank/put.test.js
+++ src/tests/integration/api/v1/bank/put.test.js
@@ -1,30 +1,36 @@
 import orchestrator from "tests/orchestrator";
+import setup from "tests/setupDatabase";
+
+const itau = "Itau";
+const nuBank = "nuBank";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.clearDatabase();
-  await fetch(`${process.env.BASE_API_URL}/bank`, {
-    method: "POST",
-    body: JSON.stringify({ name: "Itaú" }),
-  });
-  await fetch(`${process.env.BASE_API_URL}/bank`, {
-    method: "POST",
-    body: JSON.stringify({ name: "NuBank" }),
-  });
+
+  await setup.createBank(itau);
+  await setup.createBank(nuBank);
 });
 
-test("route PUT api/v1/bank/{bankId} should return a bank object updated", async () => {
-  const bankReponse = await fetch(`${process.env.BASE_API_URL}/bank`);
-  const bankResponseBody = await bankReponse.json();
-  const bankId = bankResponseBody.data[0].id;
+describe("PUT /api/v1/bank", () => {
+  describe("Anonymous user", () => {
+    test("Updating bank", async () => {
+      const bankReponse = await fetch(`${process.env.BASE_API_URL}/bank`);
+      const bankResponseBody = await bankReponse.json();
+      const bankId = bankResponseBody.data[0].id;
 
-  const updateBankData = { name: "Itaú updated" };
-  const response = await fetch(`${process.env.BASE_API_URL}/bank/${bankId}`, {
-    method: "PUT",
-    body: JSON.stringify(updateBankData),
+      const updateBankData = { name: "Itaú updated" };
+      const response = await fetch(
+        `${process.env.BASE_API_URL}/bank/${bankId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(updateBankData),
+        },
+      );
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(responseBody.name).toBe(updateBankData.name);
+    });
   });
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(200);
-  expect(responseBody.name).toBe(updateBankData.name);
 });
