257cebfc0af84b9f46ad0b4bfcf01848045f21c5
Enrico Marquez <enricovmarquezz@gmail.com>
2025-03-13 09:42:16 -0300

    refactor: use describe for more robust `extraIncome` `delete` tests

--- src/tests/integration/api/v1/extraIncome/delete.test.js
+++ src/tests/integration/api/v1/extraIncome/delete.test.js
@@ -1,46 +1,49 @@
-import setupDatabase from "tests/setupTests";
+import orchestrator from "tests/orchestrator";
+import setup from "tests/setupDatabase";
+
+const extraIncome = { name: "Freelance Job", amount: 500.0 };
+
+let bankStatementId;
 
 beforeAll(async () => {
-  await setupDatabase({
-    createMonths: ["january"],
-    createBankStatements: [{ yearId: 2025, monthId: 1 }],
-  });
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const year = 2025;
+  const january = "January";
+  const salaryAmount = 4500;
+  await setup.createYear(year);
+  await setup.createAllMonths();
+  const yearMonth = await setup.createMonthInYear(january, year);
+  const salary = await setup.createSalary(salaryAmount);
+  const bankStatement = await setup.createBankStatement(salary, yearMonth.id);
+  bankStatementId = bankStatement.id;
+  await setup.createExtraIncome(extraIncome, bankStatementId);
 });
 
-test("route DELETE api/v1/extraIncome/{extraIncomeId} should return 200 deleted", async () => {
-  const getBankStatementResponse = await fetch(
-    `${process.env.BASE_API_URL}/bankStatement?` +
-      new URLSearchParams({
-        monthId: 1,
-        yearId: 2025,
-      }),
-  );
-  const getBankStatementResponseBody = await getBankStatementResponse.json();
-  const bankStatementId = getBankStatementResponseBody.id;
+describe("DELETE /api/v1/extraIncome", () => {
+  describe("Anonymous user", () => {
+    test("route DELETE api/v1/extraIncome/{extraIncomeId} should return 200 deleted", async () => {
+      const extraIncomeResponse = await fetch(
+        `${process.env.BASE_API_URL}/extraIncome/${bankStatementId}`,
+      );
+      const extraIncomeResponseBody = await extraIncomeResponse.json();
 
-  await fetch(`${process.env.BASE_API_URL}/extraIncome/${bankStatementId}`, {
-    method: "POST",
-    body: JSON.stringify({ name: "Freelance Job", amount: 500.0 }),
+      const extraIncomeId = extraIncomeResponseBody.data[0].id;
+
+      const response = await fetch(
+        `${process.env.BASE_API_URL}/extraIncome/${extraIncomeId}`,
+        {
+          method: "DELETE",
+        },
+      );
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(responseBody.name).toBe("deleted");
+      expect(responseBody.message).toBe(
+        `value with id ${extraIncomeId} deleted successfuly`,
+      );
+    });
   });
-
-  const extraIncomeResponse = await fetch(
-    `${process.env.BASE_API_URL}/extraIncome/${bankStatementId}`,
-  );
-  const extraIncomeResponseBody = await extraIncomeResponse.json();
-
-  const extraIncomeId = extraIncomeResponseBody.data[0].id;
-
-  const response = await fetch(
-    `${process.env.BASE_API_URL}/extraIncome/${extraIncomeId}`,
-    {
-      method: "DELETE",
-    },
-  );
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(200);
-  expect(responseBody.name).toBe("deleted");
-  expect(responseBody.message).toBe(
-    `value with id ${extraIncomeId} deleted successfuly`,
-  );
 });
