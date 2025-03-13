4982a79e4fae3931cd440e4e94cc5610291ac548
Enrico Marquez <enricovmarquezz@gmail.com>
2025-03-13 15:40:43 -0300

    refactor: use describe for more robust `expense` `get` tests

--- src/tests/integration/api/v1/expense/credit/get.test.js
+++ src/tests/integration/api/v1/expense/credit/get.test.js
@@ -1,56 +1,70 @@
-import setupDatabase from "tests/setupTests";
+import orchestrator from "tests/orchestrator";
+import setup from "tests/setupDatabase";
+
+let bankStatementData;
 
 beforeAll(async () => {
-  await setupDatabase({
-    createMonths: ["january"],
-    createBankStatements: [{ yearId: 2025, monthId: 1 }],
-  });
-  await fetch(`${process.env.BASE_API_URL}/bank`, {
-    method: "POST",
-    body: JSON.stringify({ name: "Itaú" }),
-  });
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+
+  const year = 2025;
+  const january = "January";
+  const salaryAmount = 4500;
+  const bankName = "Itau";
+  await setup.createYear(year);
+  await setup.createAllMonths();
+  const yearMonth = await setup.createMonthInYear(january, year);
+  const salary = await setup.createSalary(salaryAmount);
+  const bank = await setup.createBank(bankName);
+  const bankStatement = await setup.createBankStatement(
+    salary,
+    yearMonth.id,
+    undefined,
+    [bank],
+  );
+  bankStatementData = bankStatement;
 });
 
-test("route GET api/v1/expense/credit/{expenseId} should return one expense object", async () => {
-  const bankStatementResponse = await fetch(
-    `${process.env.BASE_API_URL}/bankStatement?` +
-      new URLSearchParams({
-        monthId: 1,
-        yearId: 2025,
-      }),
-  );
-  const bankStatementResponseBody = await bankStatementResponse.json();
-  const bankStatementId = bankStatementResponseBody.id;
+describe("GET /api/v1/expense/credit", () => {
+  describe("Anonymous user", () => {
+    test("route GET api/v1/expense/credit/{expenseId} should return one expense object", async () => {
+      const expense = {
+        name: "Compra mercado",
+        description: "Compra de mercado da semana",
+        total: 543.12,
+        bankBankStatementId: bankStatementData.banks[0].id,
+      };
+      await fetch(
+        `${process.env.BASE_API_URL}/expense/credit/${bankStatementData.id}`,
+        {
+          method: "POST",
+          body: JSON.stringify(expense),
+        },
+      );
 
-  const bankResponse = await fetch(`${process.env.BASE_API_URL}/bank`);
-  const bankResponseBody = await bankResponse.json();
+      const yearMonth = {
+        month: "January",
+        year: 2025,
+      };
 
-  const expense = {
-    name: "Compra mercado",
-    description: "Compra de mercado da semana",
-    total: 543.12,
-    bankId: bankResponseBody.data[0].id,
-  };
-  await fetch(`${process.env.BASE_API_URL}/expense/credit/${bankStatementId}`, {
-    method: "POST",
-    body: JSON.stringify(expense),
+      const getBankStatementResponse = await fetch(
+        `${process.env.BASE_API_URL}/bankStatement?` +
+          new URLSearchParams(yearMonth),
+      );
+      const getBankStatementResponseBody =
+        await getBankStatementResponse.json();
+      console.log(getBankStatementResponseBody);
+
+      const response = await fetch(
+        `${process.env.BASE_API_URL}/expense/credit/${getBankStatementResponseBody.expenses[0].id}`,
+      );
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(typeof responseBody).toBe("object");
+      expect(responseBody.name).toBe(expense.name);
+      expect(responseBody.description).toBe(expense.description);
+      expect(responseBody.total).toBe(expense.total);
+    });
   });
-
-  const getExpensesListResponse = await fetch(
-    `${process.env.BASE_API_URL}/bankStatement?` +
-      new URLSearchParams({
-        monthId: 1,
-        yearId: 2025,
-      }),
-  );
-  const getExpensesListResponseBody = await getExpensesListResponse.json();
-
-  const response = await fetch(
-    `${process.env.BASE_API_URL}/expense/credit/${getExpensesListResponseBody.expenses[0].id}`,
-  );
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(200);
-  expect(typeof responseBody).toBe("object");
-  expect(responseBody.name).toBe(expense.name);
 });
