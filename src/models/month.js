365a36e6511da22665795e82b9adc065f19885ae
Enrico Marquez <enricovmarquezz@gmail.com>
2025-04-03 16:05:10 -0300

    fix: remove validations from `controller` and make `model` responsability

--- src/models/month.js
+++ src/models/month.js
@@ -1,5 +1,7 @@
 import prisma from "infra/database.js";
 import { MonthName } from "@prisma/client";
+import { ConflictError } from "errors/http";
+import { httpSuccessCreated } from "helpers/httpSuccess";
 
 async function findFirst(monthName) {
   return await prisma.month.findFirst({
@@ -21,21 +23,29 @@ async function findMany(year) {
 }
 
 async function createAllMonths() {
-  const result = await prisma.month.findMany();
-  if (result.length !== 0) {
-    return;
+  const exists = await prisma.month.findFirst();
+  if (exists) {
+    throw new ConflictError("", "all months");
   }
-  let months = [];
-  for (const [index, [, month]] of Object.entries(Object.entries(MonthName))) {
-    months.push({
-      month,
-      numeric: parseInt(index) + 1,
-    });
-  }
-  await prisma.month.createMany({
+
+  const months = createMonthsObject();
+  const result = await prisma.month.createMany({
     data: months,
   });
-  return "all months created successufuly";
+  return new httpSuccessCreated("All months created successfuly", result);
+
+  function createMonthsObject() {
+    let months = [];
+    for (const [index, [, month]] of Object.entries(
+      Object.entries(MonthName),
+    )) {
+      months.push({
+        month,
+        numeric: parseInt(index) + 1,
+      });
+    }
+    return months;
+  }
 }
 
 const month = {
--- src/pages/api/v1/month/index.js
+++ src/pages/api/v1/month/index.js
@@ -3,9 +3,7 @@ import {
   onNoMatchHandler,
   onInternalServerErrorHandler,
 } from "helpers/handlers";
-import { httpSuccessCreated } from "helpers/httpSuccess";
 import { createRouter } from "next-connect";
-import { ConflictError } from "errors/http";
 
 const router = createRouter();
 
@@ -18,10 +16,5 @@ export default router.handler({
 
 async function postHandler(req, res) {
   const result = await month.createAllMonths();
-  if (!result) {
-    const responseError = new ConflictError("", "all months");
-    return res.status(responseError.statusCode).json(responseError);
-  }
-  const responseSuccess = new httpSuccessCreated(result);
-  res.status(responseSuccess.statusCode).json(responseSuccess);
+  res.status(result.statusCode).json(result);
 }
