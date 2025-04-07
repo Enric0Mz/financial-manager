53079c4bdcaf5dc1548ea31a01158a8ddba05fd9
Enrico Marquez <enricovmarquezz@gmail.com>
2025-04-07 16:44:31 -0300

    docs: implement `swagger` documentation for `/api/v1/bankStatement/{bankStatementId}` route

--- src/pages/api/v1/bankStatement/[bankStatementId].js
+++ src/pages/api/v1/bankStatement/[bankStatementId].js
@@ -2,6 +2,52 @@ import prisma from "@infra/database";
 import { NotFoundError } from "errors/http";
 import { httpSuccessDeleted } from "helpers/httpSuccess";
 
+/**
+ * @swagger
+ * {
+ *   "/api/v1/bankStatement/{bankStatementId}": {
+ *     "delete": {
+ *       "tags": ["Bank Statement"],
+ *       "summary": "Delete bank statement",
+ *       "parameters": [
+ *         {
+ *           "name": "bankStatementId",
+ *           "in": "path",
+ *           "description": "Id of a bank statement",
+ *           "required": true,
+ *           "schema": {
+ *             "type": "integer",
+ *             "format": "int64"
+ *           }
+ *         }
+ *       ],
+ *       "responses": {
+ *         "200": {
+ *           "description": "Successful operation",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/HttpSuccess"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
+
 export default async function deleteHandler(req, res) {
   const query = req.query;
   const bankStatementId = parseInt(query.bankStatementId);
