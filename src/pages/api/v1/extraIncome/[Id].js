321c41222a54d2b70eca3d96b196a59b1d2f6de5
Enrico Marquez <enricovmarquezz@gmail.com>
2025-04-07 19:21:24 -0300

    docs: implement `swagger` documentation for `/api/v1/extraIncome/{id}` route

--- src/pages/api/v1/extraIncome/[Id].js
+++ src/pages/api/v1/extraIncome/[Id].js
@@ -17,12 +17,114 @@ export default route.handler({
   onError: onInternalServerErrorHandler,
 });
 
+/**
+ * @swagger
+ * {
+ *   "/api/v1/extraIncome/{bankStatementId}": {
+ *     "get": {
+ *       "tags": ["Extra Income"],
+ *       "summary": "List extra incomes",
+ *       "parameters": [
+ *         {
+ *           "name": "bankStatementId",
+ *           "in": "path",
+ *           "description": "Id of a bank statement",
+ *           "required": true,
+ *           "schema": {
+ *             "type": "integer",
+ *             "format": "int64"
+ *           }
+ *         }
+ *       ],
+ *       "responses": {
+ *         "200": {
+ *           "description": "Successful operation",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/ListOfExtraIncome"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
+
 async function getHandler(req, res) {
   const query = req.query;
   const bankStatementId = parseInt(query.Id);
   const result = await extraIncome.findMany(bankStatementId);
   return res.status(200).json({ data: result });
 }
+
+/**
+ * @swagger
+ * {
+ *   "/api/v1/extraIncome/{bankStatementId}": {
+ *     "post": {
+ *       "tags": ["Extra Income"],
+ *       "summary": "Create extra income",
+ *       "parameters": [
+ *         {
+ *           "name": "bankStatementId",
+ *           "in": "path",
+ *           "description": "Id of a bank statement",
+ *           "required": true,
+ *           "schema": {
+ *             "type": "integer",
+ *             "format": "int64"
+ *           }
+ *         }
+ *       ],
+ *      "requestBody": {
+ *          "description": "Extra income body",
+ *          "content": {
+ *            "application/json": {
+ *              "schema": {
+ *                "$ref": "#/components/schemas/ExtraIncomeCreate"
+ *              }
+ *            }
+ *          }
+ *        },
+ *       "responses": {
+ *         "201": {
+ *           "description": "Successful operation",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/HttpSuccess"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
 
 async function postHandler(req, res) {
   const query = req.query;
@@ -34,6 +136,62 @@ async function postHandler(req, res) {
   return res.status(result.statusCode).json(result.toJson());
 }
 
+/**
+ * @swagger
+ * {
+ *   "/api/v1/extraIncome/{extraIncomeId}": {
+ *     "patch": {
+ *       "tags": ["Extra Income"],
+ *       "summary": "Update extra income",
+ *        "requestBody": {
+ *          "description": "Extra income body",
+ *          "content": {
+ *            "application/json": {
+ *              "schema": {
+ *                "$ref": "#/components/schemas/ExtraIncomeCreate"
+ *              }
+ *            }
+ *          }
+ *        },
+ *        "parameters": [
+ *      {
+ *       "name": "extraIncomeId",
+ *        "in": "path",
+ *        "description": "Id of a extra income",
+ *        "required": true,
+ *        "schema": {
+ *          "type": "integer",
+ *          "format": "int64"
+ *          }
+ *       }
+ *     ],
+ *       "responses": {
+ *         "200": {
+ *           "description": "Successful operation",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/HttpSuccess"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
+
 async function patchHandler(req, res) {
   const query = req.query;
   const extraIncomeId = parseInt(query.Id);
@@ -43,6 +201,52 @@ async function patchHandler(req, res) {
 
   return res.status(200).json(updatedExtraIncome);
 }
+
+/**
+ * @swagger
+ * {
+ *   "/api/v1/extraIncome/{extraIncomeId}": {
+ *     "delete": {
+ *       "tags": ["Extra Income"],
+ *       "summary": "Delete extra income",
+ *       "parameters": [
+ *         {
+ *           "name": "extraIncomeId",
+ *           "in": "path",
+ *           "description": "Id of a extra income",
+ *           "required": true,
+ *           "schema": {
+ *             "type": "integer",
+ *             "format": "int64"
+ *           }
+ *         }
+ *       ],
+ *       "responses": {
+ *         "200": {
+ *           "description": "Year successfully deleted",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/HttpSuccess"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
 
 async function deleteHandler(req, res) {
   const query = req.query;
