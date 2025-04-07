de4c49ef97f2d4738f121614bcf62347e7094fe3
Enrico Marquez <enricovmarquezz@gmail.com>
2025-04-07 14:38:44 -0300

    docs: implement `swagger` documentation for `/api/v1/salary/{salaryId}` route

--- src/pages/api/v1/salary/[salaryId].js
+++ src/pages/api/v1/salary/[salaryId].js
@@ -1,5 +1,60 @@
 import salary from "models/salary";
 
+/**
+ * @swagger
+ * {
+ *   "/api/v1/salary/{salaryId}": {
+ *     "put": {
+ *       "tags": ["Salary"],
+ *       "summary": "Update salary",
+ *        "requestBody": {
+ *          "description": "Salary body",
+ *          "content": {
+ *            "application/json": {
+ *              "schema": {
+ *                "$ref": "#/components/schemas/SalaryCreate"
+ *              }
+ *            }
+ *          }
+ *        },
+ *        "parameters": [
+ *      {
+ *       "name": "salaryId",
+ *        "in": "path",
+ *        "description": "Id of a salary",
+ *        "required": true,
+ *        "schema": {
+ *          "type": "integer",
+ *          "format": "int64"
+ *          }
+ *       }
+ *     ],
+ *       "responses": {
+ *         "200": {
+ *           "description": "Successful operation",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/HttpSuccess"
+ *               }
+ *             }
+ *           }
+ *         },
+ *         "500": {
+ *           "description": "Internal server error",
+ *           "content": {
+ *             "application/json": {
+ *               "schema": {
+ *                 "$ref": "#/components/schemas/InternalServerError"
+ *               }
+ *             }
+ *           }
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ */
 export default async function putHandler(req, res) {
   const salaryId = req.query.salaryId;
   const body = req.body;
