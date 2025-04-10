5da1f68204b09c47f4eeedff91632678871f3d03
Enrico Marquez <enricovmarquezz@gmail.com>
2025-04-10 15:02:43 -0300

    fix: change fetch `debit` route to get

--- src/tests/integration/api/v1/expense/debit/get.test.js
+++ src/tests/integration/api/v1/expense/debit/get.test.js
@@ -2,6 +2,7 @@ import orchestrator from "tests/orchestrator";
 import setup from "tests/setupDatabase";
 
 let bankStatementData;
+
 const expense = {
   name: "Compra débito",
   total: 456.98,
@@ -31,15 +32,27 @@ beforeAll(async () => {
 
 describe("GET /api/v1/expense/debit", () => {
   describe("Anonymous user", () => {
-    test("Fetching debit expenses", async () => {
+    test("Getiing debit expenses", async () => {
+      const yearMonth = {
+        month: "January",
+        year: 2025,
+      };
+
+      const getBankStatementResponse = await fetch(
+        `${process.env.BASE_API_URL}/bank-statement/${yearMonth.year}?` +
+          new URLSearchParams({ month: yearMonth.month }),
+      );
+      const getBankStatementResponseBody =
+        await getBankStatementResponse.json();
+
       const response = await fetch(
-        `${process.env.BASE_API_URL}/expense/debit/${bankStatementData.id}`,
+        `${process.env.BASE_API_URL}/expense/debit/${getBankStatementResponseBody.expenses[0].id}`,
       );
       const responseBody = await response.json();
 
       expect(response.status).toBe(200);
-      expect(responseBody.data[0].name).toBe(expense.name);
-      expect(responseBody.data[0].total).toBe(expense.total);
+      expect(responseBody.name).toBe(expense.name);
+      expect(responseBody.total).toBe(expense.total);
     });
   });
 });
