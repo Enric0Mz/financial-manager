485ab91d5377b7388029a7000cb45ba51b0e3b31
Enrico Marquez <enricovmarquezz@gmail.com>
2025-05-16 14:24:39 -0300

    feat: include `tailwind` styles into healht page

--- src/pages/health/index.js
+++ src/pages/health/index.js
@@ -7,10 +7,10 @@ async function fetchAPI(key) {
 
 export default function HealthPage() {
   return (
-    <>
-      <h1>Status page</h1>
+    <div className="min-h-screen bg-primaryDark flex flex-col items-center justify-center py-10">
+      <h1 className="text-4xl font-bold text-highlight mb-8">Status Page</h1>
       <DatabaseInfo />
-    </>
+    </div>
   );
 }
 
@@ -22,19 +22,40 @@ function DatabaseInfo() {
   let dbStatusInfo;
 
   if (isLoading) {
-    dbStatusInfo = "Carregando...";
+    dbStatusInfo = (
+      <div className="bg-secondary p-4 rounded-lg shadow-lg text-highlight">
+        Carregando...
+      </div>
+    );
   }
 
   if (!isLoading && data) {
     dbStatusInfo = (
-      <>
-        <p>Última atualização: {data.updated_at}</p>
-        <p>Conexões disponíveis: {data.dependencies.database.maxConnections}</p>
-        <p>Conexões abertas: {data.dependencies.database.openedConnections}</p>
-        <p>Versão do banco de dados: {data.dependencies.database.version}</p>
-      </>
+      <div className="bg-primary p-6 border border-accent shadow-lg rounded-lg space-y-4">
+        <h1 className="text-2xl font-semibold text-highlight mb-4 border-b border-accent pb-2">
+          Database Status
+        </h1>
+
+        <p className="text-highlight">
+          <span className="font-bold">Última atualização:</span>{" "}
+          {data.updated_at}
+        </p>
+        <div className="border-t border-accent my-2"></div>
+        <p className="text-highlight">
+          <span className="font-bold">Conexões disponíveis:</span>{" "}
+          {data.dependencies.database.maxConnections}
+        </p>
+        <p className="text-highlight">
+          <span className="font-bold">Conexões abertas:</span>{" "}
+          {data.dependencies.database.openedConnections}
+        </p>
+        <p className="text-highlight">
+          <span className="font-bold">Versão do banco de dados:</span>{" "}
+          {data.dependencies.database.version}
+        </p>
+      </div>
     );
   }
 
-  return <div>{dbStatusInfo}</div>;
+  return <div className="w-full max-w-md mx-auto">{dbStatusInfo}</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDark: "#02231c",
+        primary: "#004d25",
+        secondary: "#11823b",
+        accent: "#48bf53",
+        highlight: "#91f086",
+      },
+    },
   },
   plugins: [],
 };
